f81d44d1f3e896619e5975e0d587aead2f312df3
IrinaS <ten7473@mail.ru>
2021-09-15 14:13:45 +0500

    Создан графический дизайн приложения

--- TasksListConsoleApp/TasksListWpfApp/MainWindow.xaml.cs
+++ TasksListConsoleApp/TasksListWpfApp/MainWindow.xaml.cs
@@ -24,5 +24,64 @@ namespace TasksListWpfApp
         {
             InitializeComponent();
         }
+
+        private void SelectAction_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var select = ((TextBlock)SelectAction.SelectedItem).Name;
+            /*
+            if (select == "SelectZ") ;
+            if (select == "SelectX") ;
+            if (select == "SelectC") ;
+            if (select == "SelectV") ;
+            if (select == "SelectB") ;
+            */
+        }
+               
+        private void Importance1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Importance1_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Importance2_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Importance2_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Importance3_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Importance3_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+     
+        private void SelectColor1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SelectColor2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SelectColor3_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
     }
 }
